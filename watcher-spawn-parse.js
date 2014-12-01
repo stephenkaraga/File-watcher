690c2a06aae30a32ee7bd091f29effbf53badb59
stephenkaraga <stephenkaraga@gmail.com>
2014-12-01 01:10:23 -0500

    EventEmitter with event listeneres

---
+++ watcher-spawn-parse.js
@@ -0,0 +1,22 @@
+fs = require('fs'),
+  spawn = require('child_process').spawn,
+  filename = process.argv[2];
+
+  if(!filename){
+  	throw Error("A file to watch must be specified!");
+  }
+
+fs.watch(filename, function() {
+	var
+	  ls = spawn('ls', ['-lh', filename]),
+	  output = '';
+	ls.stdout.on('data', function(chunk){
+		output += chunk.toString();
+	});
+
+	ls.on('close', function(){
+		var parts = output.split('/\s+/');
+		console.dir([parts[0], parts[4], parts[8]]);
+	});
+});
+console.log("Now watching " + filename + " for changes...");
